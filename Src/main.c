c8767e7eb5e46e83923cbe430a473feef1f5de42
Rob Riggs <rob@pangalactic.org>
2021-06-26 10:57:49 -0500

    Set I2C timing to 400kHz for EEPROM access.

--- Src/main.c
+++ Src/main.c
@@ -603,7 +603,7 @@ static void MX_I2C3_Init(void)
 {
 
   hi2c3.Instance = I2C3;
-  hi2c3.Init.Timing = 0x00000107;
+  hi2c3.Init.Timing = 0x0010061A;
   hi2c3.Init.OwnAddress1 = 0;
   hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
